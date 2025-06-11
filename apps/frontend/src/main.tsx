e8da925e9388ce1fd794952a0dc4c71554567c29
FT Tan <fangtiannn@gmail.com>
2025-06-11 12:00:54 +0800

    fix package typo

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import './index.css';
 import { queryClient } from './lib/query-client.ts';
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
